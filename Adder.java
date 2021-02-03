ab30757e4ed4ba08a8dc0041ae4583ef0b0d20fe
KaushikeePPal <kaushikee@gmail.com>
2021-02-03 13:51:46 +0530

    2nd commit with new file adder.java

---
+++ Adder.java
@@ -0,0 +1,13 @@
+/**
+ * Adder
+ */
+public class Adder {
+
+    public static void main(String[] args) {
+        
+        int num1 = 5, num2 = 15, sum;
+        sum = num1 + num2;        
+        System.out.println("Sum of these numbers: "+sum);
+      
+     }
+}
